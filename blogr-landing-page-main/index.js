75bea1be44990463d3ecf2774064090169547a49
mikeydavis123 <110591072+mikeydavis123@users.noreply.github.com>
2022-08-31 07:47:46 -0700

    Add files via upload

---
+++ blogr-landing-page-main/index.js
@@ -0,0 +1,11 @@
+document.getElementById("Contacts").addEventListener("click",arr);
+function arr(){
+    document.getElementById("clist").classList.toggle("arr");
+    document.getElementById("link2").classList.toggle("cool");
+}
+document.getElementById("ham").addEventListener("click",slide);
+function slide(){
+    document.getElementById("mobile").classList.toggle("real");
+    document.getElementById("ham").classList.toggle("real2");
+
+}
